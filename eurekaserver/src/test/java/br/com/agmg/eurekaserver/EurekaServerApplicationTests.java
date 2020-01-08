d04850632451e3285a351412622b379c9af582f1
Alexandre Guerra <guerra@wdev.int>
2020-01-08 14:28:02 -0300

    inicial

---
+++ eurekaserver/src/test/java/br/com/agmg/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agmg.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
